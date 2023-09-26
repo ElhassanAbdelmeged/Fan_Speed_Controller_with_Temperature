53dcad34ad558e8337ad3101e1439c32e629a4eb
El_Hassan_Abdel_Meged <hassanabdelmaged123@gmail.com>
2023-09-26 21:42:18 +0300

    1st version of Fan Speed Controller with Temperature Project

---
+++ Mini_Project_3_WS/Mini_Project_3/Debug/App.d
@@ -0,0 +1,16 @@
+App.o App.o: ../App.c ../motor.h ../std_types.h ../common_macros.h \
+  ../adc.h ../gpio.h ../lcd.h ../lm35_sensor.h
+
+../motor.h:
+
+../std_types.h:
+
+../common_macros.h:
+
+../adc.h:
+
+../gpio.h:
+
+../lcd.h:
+
+../lm35_sensor.h:
---
+++ Mini_Project_3_WS/Mini_Project_3/Debug/motor.d
@@ -0,0 +1,10 @@
+motor.o motor.o: ../motor.c ../PWM.h ../std_types.h ../common_macros.h \
+  ../motor.h
+
+../PWM.h:
+
+../std_types.h:
+
+../common_macros.h:
+
+../motor.h:
